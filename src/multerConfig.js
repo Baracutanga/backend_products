26cbec17e482efa9bd5223eb18e079c03d5679ea
Baracutanga <lgbr212@gmail.com>
2024-12-14 01:33:35 -0300

    Agora vai

--- src/multerConfig.js
+++ src/multerConfig.js
@@ -2,13 +2,14 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
+const pastaUpload = path.resolve(__dirname, "upload");
+
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath);
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const pastaUpload = path.resolve(__dirname, "upload");
     cb(null, pastaUpload);
   },
   filename: (req, file, cb) => {
